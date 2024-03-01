6956c9f8ad60d53fa327089e82a7ad85afdc9c67
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-01 02:17:41 +0100

    removed trailing whitespace

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_height - Calculates the height of a binary tree
  * @tree: A pointer to the root node of the tree to measure the height
- * Return: 0 if null 
+ * Return: 0 if null
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
