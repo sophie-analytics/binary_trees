3f8f2a12c805887a08087c20583fa51df4bc668c
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-03-01 02:12:36 +0100

    removed trailing white space

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - Verifies if a node is a root
  * @node: A pointer to the node to check.
- * Return: 0 is it is not a root and 1 if it is 
+ * Return: 0 is it is not a root and 1 if it is
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
